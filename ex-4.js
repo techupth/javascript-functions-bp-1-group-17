f7db3d80dfd8b9b2e3a3ff57661c3659ebd2de39
WichaponSR <111661727+WichaponSR@users.noreply.github.com>
2023-10-05 02:43:13 +0700

    Update ex-4.js

--- ex-4.js
+++ ex-4.js
@@ -1,4 +1,25 @@
 // Exercise #4 Grouping Function with Objects
 
 // Start coding here
-let calculator = {};
+let calculator = {
+ add: (a, b) => {
+    return a+b;
+ },
+
+subtract: (a, b) => {
+    return a-b;
+ },
+
+multiply: (a, b) => {
+    return a*b;
+ },
+
+divide: (a, b) => {
+    return a/b;
+ }
+};
+
+let resultAdd = calculator.add(10, 20);
+console.log(resultAdd);
+let resultDivide = calculator.divide(3000, 10);
+console.log(resultDivide);
