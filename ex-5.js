f6cad66ab49142a1053229695c81f517b5d80a8e
WichaponSR <111661727+WichaponSR@users.noreply.github.com>
2023-10-05 02:50:27 +0700

    Update ex-5.js

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,6 @@
 // Exercise #5 Min Function
 // Start coding here
-const minNumber = findMinNumber(10, 20);
-
-console.log(minNumber); // 10
+function findMinNumber(a, b) {
+    return Math.min(a, b);
+}
+console.log(findMinNumber(10, 20)); // 10
